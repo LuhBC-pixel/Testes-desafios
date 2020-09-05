0e7ba1182b88a7ace563dd80a712cecd408bcbd6
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-05 11:26:28 -0300

    Cria novas funções

---
+++ Complete a musica/musica.py
@@ -0,0 +1,15 @@
+def eh_valido(numero):
+    if (numero == '1' or numero == '2'):
+        return True
+    return False
+
+escolhe = input("Deseja jogar com a música em versão curta ou completa? (1 - curta /2 - completa): ")
+
+while not eh_valido(escolhe) or not escolhe.isnumeric():
+    print('Dígito inválido!')
+    escolhe = input("Deseja jogar com a música em versão curta ou completa? (1 - curta /2 - completa): ")
+
+if escolhe == '1':
+    print('Jogando em modo curta')
+elif escolhe == '2':
+    print('Jogando em modo completa')
