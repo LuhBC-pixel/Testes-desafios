d83bc0370bbe36be211a4a6fe513bfdc41580ab7
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-05 15:20:46 -0300

    Cria novas funções

--- Complete a musica/musicaCurta.py
+++ Complete a musica/musicaCurta.py
@@ -1,2 +1,30 @@
-arquivo = open('patinhoColoridoCurto.txt', 'r')
-print(arquivo.readline())
+def arquivo_para_lista():
+    arquivo = open('patinhoColoridoCurto.txt', 'r')
+    frases_da_musica = []
+
+    for linha in arquivo:
+        frases_da_musica.append(linha)
+    
+    return frases_da_musica
+
+lista_do_arquivo = arquivo_para_lista()
+for i in lista_do_arquivo:
+    print(i)
+
+
+# for linha in arquivo:
+#     if '_' in linha:
+#         print(linha)
+#         palavra = input(':')
+#     else:
+#         print(linha)
+
+def arquivo_resposta_para_lista():
+    arquivo = open('repostaPatinhoCurto.txt', 'r')
+    palavras_certas = []
+
+    for linha in arquivo:
+        palavras_certas.append(linha)
+    
+    return palavras_certas
+
